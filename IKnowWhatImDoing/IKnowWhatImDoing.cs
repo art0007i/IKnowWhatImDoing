80569d82dc4450dfea519ecc5cfbdfbd9d4a8121
art0007i <art0007i@gmail.com>
2025-01-31 02:04:06 +0100

    add config

--- IKnowWhatImDoing/IKnowWhatImDoing.cs
+++ IKnowWhatImDoing/IKnowWhatImDoing.cs
@@ -12,17 +12,23 @@ public class IKnowWhatImDoing : ResoniteMod
     public override string Author => "art0007i";
     public override string Version => VERSION;
     public override string Link => "https://github.com/art0007i/IKnowWhatImDoing/";
+
+    static ModConfiguration config;
+    [AutoRegisterConfigKey]
+    public static ModConfigurationKey<bool> KEY_ENABLED = new("enabled", "When true the mod is enabled", () => true);
+
     public override void OnEngineInit()
     {
+        config = GetConfiguration();
         Harmony harmony = new Harmony("me.art0007i.IKnowWhatImDoing");
         harmony.PatchAll();
-
     }
     [HarmonyPatch(typeof(WorkerInspector), "OnRemoveComponentPressed")]
     class IKnowWhatImDoingPatch
     {
         public static bool Prefix(Worker worker)
         {
+            if (!config.GetValue(KEY_ENABLED)) return true;
             Component obj = worker as Component;
             UserComponent userComponent = worker as UserComponent;
             obj?.UndoableDestroy();
